897eab9dbc5a1b5a501344104a5affe496800053
Gitiup <linx@tiup.us>
2018-03-05 18:04:25 +0800

    Update TiupException.php

--- src/Tiup/TiupException.php
+++ src/Tiup/TiupException.php
@@ -2,4 +2,11 @@
 namespace Tiup;
 class TiupException extends \Exception
 {
+    public function getData(){
+        $data = json_decode($this->message, true);
+        if(is_array($data)){
+            return $data;
+        }
+        return ['message' => $this->message];
+    }
 }
